42649264773f830875f563778ea3c402606c8492
Ajith <ajithhuggihuggi@gmail.com>
2025-01-02 19:43:18 +0530

    frontend changes

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -43,6 +43,7 @@ const ContactPage = () => {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509175!2d144.95373631531893!3d-37.81720997975125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d1c860f2e740!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1601234567890!5m2!1sen!2sau"
             width="100%"
+            title="Contact Form"
             height="300"
             style={{ border: 0 }}
             allowFullScreen=""
